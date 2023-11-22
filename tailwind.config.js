55fd219379a27acdae5493a79f6fc0a9b4b86a54
SangayThinley <20095373@tafe.wa.edu.au>
2023-11-22 14:37:09 +0800

    feat(project): index/home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
       './*.html',
   ],
   theme: {
+    colors: {
+      'froly': '#f07c8a',
+      'Pastel Pink': '#fad1da',
+      'Wax Flower': '#f0b7a4',
+      'Pancho': '#f1d1b5'
+    },
     extend: {},
   },
   plugins: [],
