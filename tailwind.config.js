a5af773706dba45dab0fa608734317114283ee06
SangayThinley <20095373@tafe.wa.edu.au>
2023-11-24 00:58:54 +0800

    feat(project):add and design about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
       './*.html',
   ],
   theme: {
     colors: {
+      ...colors,
       'froly': '#f07c8a',
       'Pastel Pink': '#fad1da',
       'Wax Flower': '#f0b7a4',
