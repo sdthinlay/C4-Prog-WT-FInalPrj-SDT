78da3b0763ef9d160f583cba91cb13183a8587de
SangayThinley <20095373@tafe.wa.edu.au>
2023-11-24 01:38:06 +0800

    feat(project): add new pic

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
     colors: {
       ...colors,
       'froly': '#f07c8a',
-      'Pastel Pink': '#fad1da',
-      'Wax Flower': '#f0b7a4',
-      'Pancho': '#f1d1b5'
+      'Pancho': '#f1d1b5',
+      'Wax-Flower': '#f0b7a4',
+      'Pastel-Pink': '#fad1da'
     },
     extend: {},
   },
